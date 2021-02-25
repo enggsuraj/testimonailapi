ae278f829ef3ceda24eaaa7b5bff36b67c607049
Suraj <info.blogtheorem@gmail.com>
2021-02-25 20:17:49 +0530

    commit

--- components/EntireJSON.js
+++ components/EntireJSON.js
@@ -37,6 +37,7 @@ function EntireJSON() {
               <div className="bg-gray-100 rounded flex p-4 h-full items-center hover:bg-blue-50">
                 <svg
                   fill="none"
+                  stroke="currentColor"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="3"
@@ -57,6 +58,7 @@ function EntireJSON() {
               <div className="bg-gray-100 rounded flex p-4 h-full items-center hover:bg-blue-50">
                 <svg
                   fill="none"
+                  stroke="currentColor"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="3"
@@ -79,9 +81,9 @@ function EntireJSON() {
                 <svg
                   fill="none"
                   stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="3"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="3"
                   className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
                   viewBox="0 0 24 24"
                 >
